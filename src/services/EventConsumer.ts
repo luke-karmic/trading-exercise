5654dd9ec7a08737371cb3983c9a7568bb6452fe
luke-karmic <luketaaffe.se@gmail.com>
2025-06-05 21:56:35 +0100

    (feat) handle consumers more effectively

--- src/services/EventConsumer.ts
+++ src/services/EventConsumer.ts
@@ -1,43 +1,40 @@
+import { EventEmitter } from 'events';
 import { EVENT_NAMES } from '../types/events';
 import { PriceUpdateConsumer } from './consumers/PriceUpdateConsumer';
 import { TradeExecutionConsumer } from './consumers/TradeExecutionConsumer';
 import { StopLossOrderConsumer } from './consumers/StopLossOrderConsumer';
+import { PriceUpdate, TradeExecution, StopLossOrder } from '../types/events';
+
+type EventTypes = PriceUpdate | TradeExecution | StopLossOrder;
+type ConsumerTypes = PriceUpdateConsumer | TradeExecutionConsumer | StopLossOrderConsumer;
+
+type ConsumerMap = {
+  [EVENT_NAMES.PRICE_UPDATE]: PriceUpdateConsumer;
+  [EVENT_NAMES.TRADE_EXECUTION]: TradeExecutionConsumer;
+  [EVENT_NAMES.STOP_LOSS_ORDER]: StopLossOrderConsumer;
+};
 
 /**
  * DDD: Domain Driven Design
  * This class represents a decoupled component for handling events from the event bus.
  * Using AVRO schema to deserialize the events (realistic queue)
  */
-export class EventConsumer {
-  private priceUpdateConsumer: PriceUpdateConsumer;
-  private tradeExecutionConsumer: TradeExecutionConsumer;
-  private stopLossOrderConsumer: StopLossOrderConsumer;
+export class EventConsumer extends EventEmitter {
+  private consumers: Map<keyof ConsumerMap, ConsumerTypes>;
 
   constructor() {
-    this.priceUpdateConsumer = new PriceUpdateConsumer();
-    this.tradeExecutionConsumer = new TradeExecutionConsumer();
-    this.stopLossOrderConsumer = new StopLossOrderConsumer();
-
-    [this.priceUpdateConsumer, this.tradeExecutionConsumer, this.stopLossOrderConsumer].forEach(consumer => {
-      consumer.on(EVENT_NAMES.EVENT, (event) => {
-        this.handleEvent(event.type, event.buffer);
-      });
-    });
+    super();
+    this.consumers = new Map([
+      [EVENT_NAMES.PRICE_UPDATE, new PriceUpdateConsumer()],
+      [EVENT_NAMES.TRADE_EXECUTION, new TradeExecutionConsumer()],
+      [EVENT_NAMES.STOP_LOSS_ORDER, new StopLossOrderConsumer()]
+    ] as [keyof ConsumerMap, ConsumerTypes][]);
   }
 
   public handleEvent(type: string, buffer: Buffer): void {
-    switch (type) {
-      case EVENT_NAMES.PRICE_UPDATE:
-        this.priceUpdateConsumer.handleEvent(type, buffer);
-        break;
-      case EVENT_NAMES.TRADE_EXECUTION:
-        this.tradeExecutionConsumer.handleEvent(type, buffer);
-        break;
-      case EVENT_NAMES.STOP_LOSS_ORDER:
-        this.stopLossOrderConsumer.handleEvent(type, buffer);
-        break;
-      default:
-        console.warn(`Unknown event type: ${type}`);
+    const consumer = this.consumers.get(type as keyof ConsumerMap);
+    if (consumer) {
+      consumer.handleEvent(type, buffer);
     }
   }
 } 
